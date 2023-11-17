9dbbcb463f44d6874b8914521050d3a6f0466ae5
Aliyev Siyab <100992@mail.ru>
2023-11-17 11:27:38 +0100

    Add Object,x and y.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        int y = 5;
+        Orange orange = new Orange();
+        int x = 10;
+        System.out.println(x + y);
+
     }
 }
